cf4bf4a7662556ecdce22857aa2608f78287f7f1
Johson Rodriguez <sukerjc@gmail.com>
2020-11-05 22:54:47 -0500

    corrections

--- classdojo/src/main/java/com/classdojo/app/ClassdojoApplication.java
+++ classdojo/src/main/java/com/classdojo/app/ClassdojoApplication.java
@@ -1,13 +1,13 @@
-package com.dojo;
+package com.classdojo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DojoApplication {
+public class ClassdojoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DojoApplication.class, args);
+		SpringApplication.run(ClassdojoApplication.class, args);
 	}
 
 }
--- classdojo/src/test/java/com/classdojo/app/ClassdojoApplicationTests.java
+++ classdojo/src/test/java/com/classdojo/app/ClassdojoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dojo;
+package com.classdojo.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DojoApplicationTests {
+class ClassdojoApplicationTests {
 
 	@Test
 	void contextLoads() {
