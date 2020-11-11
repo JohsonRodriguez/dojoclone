53b63babbd2959260f722110f9c74be0664888df
Johson Rodriguez <sukerjc@gmail.com>
2020-11-10 22:01:24 -0500

    noo

--- login/src/main/java/com/login/app/service/UserServiceImpl.java
+++ login/src/main/java/com/login/app/service/UserServiceImpl.java
@@ -1,5 +1,37 @@
 package com.login.app.service;
 
-public class UserServiceImpl {
+import java.util.Arrays;
+
+
+import org.springframework.stereotype.Service;
+
+import com.login.app.model.Role;
+import com.login.app.model.User;
+import com.login.app.repository.UserRepository;
+import com.login.app.web.dto.UserRegistrationDto;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	//@Autowired
+	private UserRepository userRepository;
+	
+	
+	public UserServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+
+
+	@Override
+	public User save(UserRegistrationDto registrationDto) {
+		User user = new User(registrationDto.getFirstName(), 
+				registrationDto.getLastName(), 
+				registrationDto.getEmail(), 
+				registrationDto.getPassword(), 
+				Arrays.asList(new Role("ROLE_USER")));
+		return userRepository.save(user);
+		
+	}
 
 }
